b61fb1859ffad7a03536e280174317bdf8cbe806
Chirag Aggarwal <chi6rag@gmail.com>
2015-01-13 13:49:20 +0530

    Implement create articles functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,20 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params_article)
+    if @article.save
+      redirect_to articles_url
+    else
+      redirect_to new_article_path
+    end
   end
 
   def show
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,5 @@
 module ArticlesHelper
+  def params_article
+    params.require(:article).permit(:title, :body);
+  end
 end
