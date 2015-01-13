0f70a6eb90d97d5b2367afd4badde303d2261abe
Chirag Aggarwal <chi6rag@gmail.com>
2015-01-13 14:37:06 +0530

    Implement delete and edit functionality; Add form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,4 +20,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.delete
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(params_article)
+    redirect_to articles_path
+  end
 end
