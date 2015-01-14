df6d20fbb8925256b1a93c85346247a3291642e1
Chirag Aggarwal <chi6rag@gmail.com>
2015-01-14 16:49:46 +0530

    Add tags controller; Show tags on article page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'git/status'
+
   root 'articles#index'
   resources :articles do # This gives the REST routes for articles to the application
     resources :comments  # This gives article_comments_ path/url
   end
+  resources :tags
 end
