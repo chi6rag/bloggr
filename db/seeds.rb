f95e3fae46aec71c4dc2f89e008bebd1ebc6da4f
Chirag Aggarwal <chi6rag@gmail.com>
2015-01-14 11:14:16 +0530

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+a = Article.new(title: 'Letterpress Cheater Algorithm', body: 'A few months ago when Letterpress was hot, I thought about an efficient algorithm for a cheater application. A trivial solution in Ruby is quite slow for this problem, which motivated me to check out alternative approaches. Abbreviated, the problem is the following: Given 25 random letters (letters), find every string in an array of strings (words) that consists of only those letters. I’ll start by walking through a naive solution before presenting a data structure to solve this problem efficiently.')
+
+a.save
+
+b = Article.new(title: 'The 30 most productive days of my life', body: 'It’s scary as it feels like it’s whatever you come down to. When you give everything you have, you find yourself in a paradoxical state of weakness. What if the result of your absolute max is disappointing? After my home-spun philosophical observations – I decided to give the challenge a fair go. For the challenge, I jotted down the things I wanted to achieve:')
+
+b.save
+
+a.comments.new(author_name: 'Chirag Aggarwal', body: 'wow! impressive!').save
+b.comments.new(author_name: 'Chirag Aggarwal', body: 'I am for sure gonna follow that!').save
