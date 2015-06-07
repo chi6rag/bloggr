eb010c64c85aab7c949056a5b291d53eb509c40c
Chirag Aggarwal <chi6rag@gmail.com>
2015-06-07 17:23:44 +0530

    Complete index and show actions for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-	get '/articles', to: 'articles#index', as: 'articles'
-	post '/articles', to: 'articles#create'
-	get '/articles/new', to: 'articles#new', as: 'new_article'
-	get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
-	get '/articles/:id', to: 'articles#show', as: 'article'
-	put 'articles/:id', to: 'articles#update'
-	patch 'articles/:id', to: 'articles#update'
-	delete 'articles/:id', to: 'articles#destroy'
+	
+		get '/articles', to: 'articles#index', as: 'articles'
+		post '/articles', to: 'articles#create'
+		get '/articles/new', to: 'articles#new', as: 'new_article'
+		get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+		get '/articles/:id', to: 'articles#show', as: 'article'
+		put 'articles/:id', to: 'articles#update'
+		patch 'articles/:id', to: 'articles#update'
+		delete 'articles/:id', to: 'articles#destroy'
+			
 end
